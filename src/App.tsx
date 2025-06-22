f095d934aae48a3e777ccc89612f45d590c91e3b
eugenestepanyuk <erl.evgeniy@gmail.com>
2025-06-22 18:08:20 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,95 @@
+import "./App.css";
+import { Collapsible } from "./components/collapsible";
+
+function App() {
+  return (
+    <>
+      <Collapsible />
+      <Collapsible>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni impedit
+        voluptates officiis ipsam incidunt minus explicabo quasi natus
+        dignissimos eveniet?
+      </Collapsible>
+      <Collapsible width={200}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni impedit
+        voluptates officiis ipsam incidunt minus explicabo quasi natus
+        dignissimos eveniet?
+      </Collapsible>
+      <Collapsible>
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum sint,
+        voluptatum perferendis harum laborum natus veniam vero error saepe a?
+        Repellendus quasi iusto fuga eos voluptate iste itaque velit rerum
+        possimus corrupti, molestias exercitationem praesentium labore ad
+        deleniti nam nesciunt commodi perferendis. Tempore rerum blanditiis
+        aspernatur incidunt? Cumque, autem! Animi aperiam ad exercitationem sint
+        officiis veniam quo obcaecati perspiciatis debitis inventore error natus
+        minima soluta molestiae laudantium voluptates eveniet atque, ipsum quas
+        expedita ipsam laboriosam libero! Sunt assumenda quod suscipit veritatis
+        iure aliquid est asperiores esse nemo nulla quas praesentium earum,
+        error commodi neque distinctio ullam delectus, numquam recusandae ipsa
+        facilis? Omnis nostrum, unde, odit aperiam vel voluptatibus officia
+        optio harum recusandae alias incidunt illo numquam debitis velit, fugit
+        tempora eos repellendus eveniet laboriosam perferendis ducimus illum
+        magnam libero! Totam quis fugiat suscipit voluptatem exercitationem?
+        Quasi odio nostrum laborum cumque perspiciatis modi earum ea consectetur
+        excepturi facilis deleniti unde, perferendis voluptate maiores, alias,
+        laboriosam iure eos cupiditate harum cum natus enim laudantium
+        repellendus inventore. Perferendis iure reprehenderit cumque fugiat
+        nulla deserunt est temporibus quos labore, fugit libero quaerat! Placeat
+        provident animi tempore minima, necessitatibus pariatur, asperiores
+        quasi debitis corrupti blanditiis impedit! Laboriosam maxime
+        consequuntur tenetur placeat doloribus earum vel sunt?
+      </Collapsible>
+      <Collapsible>{[<div key={1}>Item 1</div>]}</Collapsible>
+      <Collapsible>
+        {[
+          <button key={1}>Button 1</button>,
+          <button key={2}>Button 2</button>,
+          <button key={3}>Button 3</button>,
+        ]}
+      </Collapsible>
+      <table style={{ width: "100%", marginTop: 20, tableLayout: "fixed" }}>
+        <colgroup>
+          <col style={{ width: "200px" }} />
+          <col style={{ minWidth: "200px" }} />
+        </colgroup>
+        <tbody>
+          <tr>
+            <td style={{ verticalAlign: "top", padding: 6 }}>
+              <Collapsible>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+                impedit voluptates officiis ipsam incidunt minus explicabo quasi
+                natus dignissimos eveniet?
+              </Collapsible>
+            </td>
+            <td style={{ verticalAlign: "top", padding: 6 }}>
+              <Collapsible>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+                impedit voluptates officiis ipsam incidunt minus explicabo quasi
+                natus dignissimos eveniet?
+              </Collapsible>
+            </td>
+          </tr>
+          <tr>
+            <td style={{ verticalAlign: "top", padding: 6 }}>
+              <Collapsible>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+                impedit voluptates officiis ipsam incidunt minus explicabo quasi
+                natus dignissimos eveniet?
+              </Collapsible>
+            </td>
+            <td style={{ verticalAlign: "top", padding: 6 }}>
+              <Collapsible>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+                impedit voluptates officiis ipsam incidunt minus explicabo quasi
+                natus dignissimos eveniet?
+              </Collapsible>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/collapsible.tsx
@@ -0,0 +1,111 @@
+import React, { useState, useRef, useEffect } from "react";
+import type { ReactNode, ReactElement } from "react";
+import styles from "./Collapsible.module.scss";
+
+interface CollapsibleProps {
+  children?: ReactNode;
+  width?: string | number;
+}
+
+export const Collapsible: React.FC<CollapsibleProps> = ({
+  children,
+  width,
+}) => {
+  const [expanded, setExpanded] = useState<boolean>(false);
+  const [showToggle, setShowToggle] = useState<boolean>(false);
+  const textRef = useRef<HTMLDivElement>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  const isText = typeof children === "string";
+  const isArray = Array.isArray(children);
+  const isNull = children === null || children === undefined;
+
+  useEffect(() => {
+    const updateShowToggle = (): void => {
+      if (isText && textRef.current && wrapperRef.current) {
+        const text = textRef.current;
+        const container = wrapperRef.current;
+        setShowToggle(text.scrollWidth > container.clientWidth);
+      }
+
+      if (isArray) {
+        const validElements = (children as ReactElement[]).filter(Boolean);
+        setShowToggle(validElements.length > 1);
+      }
+    };
+
+    updateShowToggle();
+
+    const resizeObserver = new ResizeObserver(updateShowToggle);
+    if (wrapperRef.current) {
+      resizeObserver.observe(wrapperRef.current);
+    }
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, [children, isText, isArray]);
+
+  if (isNull) return null;
+
+  const handleToggle = (): void => setExpanded((prev) => !prev);
+
+  const containerStyle: React.CSSProperties =
+    typeof width === "number" ? { width: `${width}px` } : { width };
+
+  return (
+    <div className={styles.wrapper} style={containerStyle} ref={wrapperRef}>
+      {isText && (
+        <div className={styles.rowLayout}>
+          <div
+            className={styles.textContent}
+            ref={textRef}
+            style={{
+              whiteSpace: expanded ? "normal" : "nowrap",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              flex: 1,
+            }}
+          >
+            {children}
+          </div>
+          {(showToggle || expanded) && (
+            <button className={styles.toggleIconBtn} onClick={handleToggle}>
+              <span className={expanded ? styles.iconUp : styles.iconDown} />
+            </button>
+          )}
+        </div>
+      )}
+
+      {isArray && (
+        <div className={styles.listWrapper}>
+          <div className={styles.rowLayout}>
+            <div className={styles.childItem}>
+              {(children as ReactElement[]).filter(Boolean)[0]}
+            </div>
+            {showToggle && (
+              <button className={styles.toggleIconBtn} onClick={handleToggle}>
+                <span className={expanded ? styles.iconUp : styles.iconDown} />
+              </button>
+            )}
+          </div>
+
+          {expanded && (
+            <div className={styles.restList}>
+              {(children as ReactElement[])
+                .filter(Boolean)
+                .slice(1)
+                .map((child, index) => (
+                  <div key={index} className={styles.childItem}>
+                    {child}
+                  </div>
+                ))}
+            </div>
+          )}
+        </div>
+      )}
+
+      {!isText && !isArray && <div>{children}</div>}
+    </div>
+  );
+};
